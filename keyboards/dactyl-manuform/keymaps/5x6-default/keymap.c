78da4eb344c35223e71e9cb6cbfd519edd4eb9f0
David Hammer <d4vid.h4mm3r@gmail.com>
2018-01-12 23:33:09 +0100

    Working on a colemak-ish layout

--- keyboards/dactyl-manuform/keymaps/5x6-default/keymap.c
+++ keyboards/dactyl-manuform/keymaps/5x6-default/keymap.c
@@ -18,31 +18,32 @@ extern keymap_config_t keymap_config;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = KEYMAP_5x6(
   // left hand
-   KC_ESC,    KC_1,    KC_2,    KC_3,   KC_4,   KC_5, 
-   KC_GRV,    KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,
-   KC_TAB,    KC_A,    KC_S,    KC_D,   KC_F,   KC_G,
+   KC_GRV,    KC_1,    KC_2,    KC_3,   KC_4,   KC_5,
+   KC_TAB,    KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,
+   KC_ESC,    KC_A,    KC_R,    KC_S,   KC_T,   KC_D,
    KC_LSHIFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
                        KC_TAB,  KC_BSLS,
-                               LT(_FN, KC_DEL), KC_LSFT, 
-                               KC_SPC,   KC_LCTRL,
-                               KC_ENTER, KC_LALT,
+                                  KC_LCTRL, KC_SPC,
+                                       KC_ENTER, KC_BSPC,
+                                       KC_ENTER, KC_LALT,
         // right hand
                      KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,
-                     KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_EQL,
-                     KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, 
-                     KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSHIFT,
-                                      KC_LBRC, KC_RBRC,
-        KC_SPC, LT(_FN, KC_BSPC), 
-        CTL_T(KC_PGUP), KC_PGDN,
-        KC_LALT, KC_LGUI),
+                     KC_J,    KC_L,   KC_U,    KC_Y,    KC_SCLN, KC_EQL,
+                     KC_H,    KC_N,   KC_E,    KC_I,    KC_O,    KC_QUOT,
+                     KC_K,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSHIFT,
+                                         KC_LBRC, KC_RBRC,
+                     KC_SPC, KC_RALT,
+         KC_DEL, KC_LGUI,
+         TO(_FN), KC_LGUI
+                       ),
 [_FN] = KEYMAP_5x6(
   // left hand
-   _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5, 
+   _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,
    _______,  _______,  KC_HOME,  KC_UP,    KC_END,   KC_PGUP,
    _______,  _______,  KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_PGDN,
    RESET,    _______,  _______,  _______,  _______,  _______,
-                       _______,  _______, 
-                               LT(_FN, KC_DEL), KC_LSFT, 
+                       _______,  _______,
+                               LT(_FN, KC_DEL), KC_LSFT,
                                KC_SPC,   KC_LCTRL,
                                KC_ENTER, KC_LALT,
         // right hand
@@ -51,9 +52,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RIGHT, _______, _______,
                      KC_CALC,  KC_MAIL,  KC_MYCM,  KC_WHOM,  _______,   RESET,
                                          _______,  _______,
-        KC_SPC, LT(_FN, KC_BSPC), 
+               KC_SPC, LT(_FN, KC_BSPC),
         CTL_T(KC_PGUP), KC_PGDN,
-        KC_LALT, KC_LGUI),
+        TO(_QWERTY), KC_LGUI),
 };
 
 
