05f8887221a51802289d768734c27755b72813ba
David Hammer <d4vid.h4mm3r@gmail.com>
2018-01-25 21:22:23 +0100

    Adding COLEMAK layer for other computers

--- keyboards/dactyl-manuform/keymaps/5x6-default/keymap.c
+++ keyboards/dactyl-manuform/keymaps/5x6-default/keymap.c
@@ -10,6 +10,7 @@ extern keymap_config_t keymap_config;
 // entirely and just use numbers.
 #define _QWERTY 0
 #define _FN   1
+#define _COLE 2
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
@@ -30,12 +31,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,
                      KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_EQL,
                      KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-                     KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RALT,
+                     KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_LALT,
                                          KC_LBRC, KC_RBRC,
                      KC_SPC, KC_RSHIFT,
          KC_DEL,  KC_LGUI,
-         TO(_FN), _______
-                       ),
+         _______, TO(_FN)),
 [_FN] = KEYMAP_5x6(
   // left hand
    _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,
@@ -54,7 +54,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                          _______,  _______,
                KC_SPC, LT(_FN, KC_BSPC),
         CTL_T(KC_PGUP), KC_PGDN,
-        TO(_QWERTY), KC_LGUI),
+        TO(_QWERTY), TO(_COLE)),
+[_COLE] = KEYMAP_5x6(
+  // left hand
+   KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,
+   KC_TAB,  KC_Q,    KC_W,    KC_F,   KC_P,   KC_G,
+   KC_ESC,  KC_A,    KC_R,    KC_S,   KC_T,   KC_D,
+   KC_LCTL, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
+                       _______,  KC_BSLS,
+                                  KC_LSHIFT, KC_SPC,
+                                       KC_ENTER, KC_BSPC,
+                                       KC_ENTER, KC_LALT,
+        // right hand
+                     KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,
+                     KC_J,    KC_L,   KC_U,    KC_Y,    KC_SCLN, KC_EQL,
+                     KC_H,    KC_N,   KC_E,    KC_I,    KC_O,    KC_QUOT,
+                     KC_K,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_LALT,
+                                         KC_LBRC, KC_RBRC,
+                     KC_SPC, KC_RSHIFT,
+         KC_DEL,  KC_LGUI,
+         TO(_FN), _______),
 };
 
 
